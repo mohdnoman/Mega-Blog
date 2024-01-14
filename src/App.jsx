b2e5f8de9caa066bac4ef5ad0d330164bfb0a1f2
mohd noman <mohdnoman2751@gmail.com>
2024-01-14 23:54:38 +0530

    created project structure.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>blog app with appwrite</h1>
+    <h1>{import.meta.env.VITE_APPWRITE_URL}</h1>
+    </>
+  )
+}
+
+export default App
